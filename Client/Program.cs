8c431b436dc153b07c44134b397ee74608cc32ce
korolslava <sviatoslav.motalo@gmail.com>
2026-05-15 17:55:56 +0300

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+const int port = 5000;
+
+var client = new TcpClient();
+client.Connect(IPAddress.Loopback, port);
+
+var stream = client.GetStream();
+
+var reader = Task.Run(() =>
+{
+    var buffer = new byte[1024];
+
+    while (true)
+    {
+        int bytesRead;
+
+        try
+        {
+            bytesRead = stream.Read(buffer, 0, buffer.Length);
+        }
+        catch
+        {
+            Console.WriteLine("Connection broken.");
+            break;
+        }
+
+        if (bytesRead == 0)
+        {
+            Console.WriteLine("Server disconnected.");
+            break;
+        }
+
+        var message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+        Console.WriteLine();
+        Console.WriteLine($"Server: {message}");
+        Console.Write("Client: ");
+    }
+});
+
+while (true)
+{
+    Console.Write("Client: ");
+    var message = Console.ReadLine();
+
+    var messageBytes = Encoding.UTF8.GetBytes(message!);
+    stream.Write(messageBytes, 0, messageBytes.Length);
+
+    if (message == "exit")
+    {
+        Console.WriteLine("Chat ended.");
+        break;
+    }
+}
+
+stream.Close();
+client.Close();
---
+++ Server/Program.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Concurrent;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+const int port = 5000;
+
+var listener = new TcpListener(IPAddress.Loopback, port);
+listener.Start();
+
+Console.WriteLine($"Server started on {IPAddress.Loopback}:{port}");
+
+var cts = new CancellationTokenSource();
+
+var clients = new ConcurrentDictionary<Guid, TcpClient>();
+
+Task.Run(() =>
+{
+    while (true)
+    {
+        var client = listener.AcceptTcpClient();
+        Task.Run(() => HandleClient(client));
+    }
+}, cts.Token);
+
+Console.Write("Server: ");
+var message = Console.ReadLine();
+
+if (message == "exit")
+{
+    Console.WriteLine("Chat ended.");
+    cts.Cancel();
+}
+
+listener.Stop();
+
+void HandleClient(TcpClient client)
+{
+    Console.WriteLine();
+    Console.WriteLine("Client Connected.");
+    Console.Write("Server: ");
+
+    clients.TryAdd(Guid.NewGuid(), client);
+
+    try
+    {
+        var stream = client.GetStream();
+
+        SendToClient(stream, "Welcome to the chat!");
+        SendToClient(stream, "Use: '/online' to see who's online.");
+
+        while (true)
+        {
+            var response = ReadFromClient(stream);
+
+            if (response == "/online")
+            {
+                SendToClient(stream, $"Online: {Environment.NewLine}{string.Join(Environment.NewLine, clients.Keys)}");
+            }
+
+            
+        }
+    }
+    catch
+    {
+
+    }
+}
+
+void SendToClient(NetworkStream stream, string message)
+{
+    var data = Encoding.UTF8.GetBytes(message);
+    stream.Write(data, 0, data.Length);
+}
+
+string ReadFromClient(NetworkStream stream)
+{
+    var buffer = new byte[1024];
+
+    var read = stream.Read(buffer, 0, buffer.Length);
+
+    if (read == 0)
+    {
+        return null;
+    }
+
+    return Encoding.UTF8.GetString(buffer, 0, read);
+}
